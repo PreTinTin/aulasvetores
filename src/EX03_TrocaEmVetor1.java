43b6e6a572bb85724a85f1ec364e6904449198ca
PreTinTin <79773858+PreTinTin@users.noreply.github.com>
2021-04-06 18:46:07 -0300

    Update EX03_TrocaEmVetor1.java

--- src/EX03_TrocaEmVetor1.java
+++ src/EX03_TrocaEmVetor1.java
@@ -5,19 +5,30 @@ public class EX03_TrocaEmVetor1 {
 	public static void main(String[] args) {
 
 		int [] N = new int [20];
+		int T; 
+		int pos = 1;
 		
 		Scanner poze = new Scanner(System.in);
 		
-		for (int i = N.length - 1; i >= 0; i--)
-			{
-				N[i] = poze.nextInt();
-			}
-		
-		for (int i = 0; i < N.length; i++)
-			{
-				System.out.println("N[" + i + "] = " + N[i]);
-			}
-
-	}
+		for(int i = 0; i < N.length; i++) 
+		{
+            N[i]= poze.nextInt();
+        }
+        
+        for(int i = 0; i < (N.length/2); i++) 
+        {          
+                 T = N[i];
+                 N[i] = N[N.length - pos];
+                 N[N.length - pos] = T;
+            
+            pos++;
+        }
+        
+        for(int i = 0; i < N.length; i++) 
+        {
+            System.out.println("N[" + i + "] = " + N[i]);
+        }
+        
+    }
 
 }
