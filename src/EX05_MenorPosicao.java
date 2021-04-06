7d06f11dbfb4551ffd93d1698abe832cceebf72a
PreTinTin <79773858+PreTinTin@users.noreply.github.com>
2021-04-06 18:45:39 -0300

    Update EX05_MenorPosicao.java

--- src/EX05_MenorPosicao.java
+++ src/EX05_MenorPosicao.java
@@ -4,41 +4,36 @@ public class EX05_MenorPosicao {
 
 	public static void main(String[] args) {
 		
-		int N = 0;
-		int pos = 0, menor = 0;		
-		
-		Scanner poze = new Scanner(System.in);
-		
-		do {
-			N = poze.nextInt();
-		}while ((N <= 1) || (N >= 1000));
-		
-		int [] X = new int [N];
-		
-		
-		for (int i = 0; i < X.length; i++)
-		{
-			X[i] = poze.nextInt();
-		
-		}
-		
-		for (int i = 0; i < X.length; i++)
-			{
-				if (menor == 0)
-				{
-					menor = X[i];		
-				}
-				if (menor > X[i])
-				{
-					menor = X[i];
-					pos = i;
-				}
-				
-			}
-		
-		System.out.println("Menor valor: " + menor + "\n" + "Posicao: " + pos);
-		
-
-	}
+        int N; 
+        int menor = 0; 
+        int pos = 0;     
+        
+        Scanner poze = new Scanner(System.in);
+        
+        N = poze.nextInt();
+        
+        int [] X = new int [N];
+        
+        for(int i = 0; i < X.length; i++) 
+        {
+            X[i] = poze.nextInt();
+        }
+        
+        for(int i = 0; i < X.length; i++) 
+        {
+            if(i == 0) 
+            {
+            menor = X[i];
+            }
+            
+            if(X[i] < menor) 
+            {
+                menor = X[i];
+                pos = i;
+            }
+        }
+        
+        System.out.println("Menor valor: " + menor + "\n" + "Posicao: " + pos);
+    }
 
 }
